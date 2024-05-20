846ed4ec70ff02b2acaae52cbfe34c15fe69f88b
Ingeon Hwang <165127332+kikisushi@users.noreply.github.com>
2024-05-20 21:40:02 +0900

    [level 1] Title: 내적, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,11 +1,4 @@
 function solution(a, b) {
-   
-    let answer = 0;
-     //1. for문
-    for(i=0; i<a.length; i++){
-        //2. a와 b의 곱한 값을 더하기등호 연산자로 쌓기
-       answer += a[i]*b[i]
-    }
-    
-    return answer;
+return  a.reduce((acc,cur,idx) => acc+ cur*b[idx], 0)
+
 }
